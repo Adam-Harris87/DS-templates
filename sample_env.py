b580160ac23a14b89840a9c4b629bbcff1e57266
Adam Harris <afharris87@gmail.com>
2023-05-07 22:11:08 -0500

    adding explore functions

--- sample_env.py
+++ sample_env.py
@@ -1,7 +1,11 @@
+# you will need to change these variables to your own username and password
+# along with the hostname of the sql server that you are connecting to
 user = 'your_user_name'
 password = 'your_password'
 host = 'data.youWant.com'
 
 def get_db_url(db_name, host=host, user=user, password=password):
-    url = f'mysql+pymysql://{user}:{password}@{host}/{db_name}'
-    return url
+    '''
+    This will return a connection url string for connecting to the specified sql server
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db_name}'
